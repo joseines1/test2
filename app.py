bc1fd34a915227e7cb530dd3e116fbb24d7f49d8
joseines1 <joseolivo78376@gmail.com>
2024-09-09 18:43:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def alumnos():
 
 @app.route("/alumnos/guardar", methods=["POST"])
 def alumnosGuardar():
-    matricula       =  {request.form["txtMatriculaFA"]
-    nombreapellido  =  {request.form["txtNombreApellidoFA"]
+    matricula       =  request.form["txtMatriculaFA"]
+    nombreapellido  =  request.form["txtNombreApellidoFA"]
     return f"Matrícula: {matricula}  Nombre y Apellido: {nombreapellido} "
