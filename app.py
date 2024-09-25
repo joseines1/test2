b9df1cf75e0bb0d5c5da61f09b641628ee48310b
joseines1 <joseolivo78376@gmail.com>
2024-09-24 19:00:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ pusher_client = pusher.Pusher(
 #def index():
  #   return "<p>Hola, Mundo!</p>"
 
-@app.route("/alumnos")
+@app.route("/")
 def index():
     cursor = con.cursor(dictionary=True)
     
