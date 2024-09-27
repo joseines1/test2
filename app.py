80375926eafe99857f98155c08ca1c64263c674b
joseines1 <joseolivo78376@gmail.com>
2024-09-27 17:23:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ pusher_client = pusher.Pusher(
 #def index():
  #   return "<p>Hola, Mundo!</p>"
 
-@app.route("/app")
-def app():
+@app.route("/")
+def index():
     cursor = con.cursor(dictionary=True)
     
     # Consulta a la base de datos
